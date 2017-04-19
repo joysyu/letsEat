db1d44bb4f56a6448957083419c435bd84c80fee
Rena Liu <rliu14@mit.edu>
2017-04-18 22:15:06 -0400

    added notifications for wrong password

--- login.js
+++ login.js
@@ -7,19 +7,27 @@ $(document).on("click", ".login.button", function(evt) {
 });
 
 $(document).on("click", '.circular.ui.icon.button', function(evt) {
-	console.log(evt.target.id)
+	var button_id = evt.target.id
+	$('#' + button_id).css({'background': 'red'})
+	$('#' + button_id).disabled = true
 	if (order == undefined) {
+		$('#wrong-password').html("");
 		order = evt.target.id
 	} else {
 		order += evt.target.id
 	}
 
 	if (order.length == 4) {
-		console.log("ORDER", order)
 		if (order == password) {
-			window.location.href='index.html';
+			$('.circular.ui.icon.button').css({'background': 'green'})
+			setTimeout(function () {
+				window.location.href='index.html';
+			}, 500)
 		} else {
 			order = undefined
+			$('#wrong-password').html("<span> Wrong password! Please try again! </span>");
+			$('.circular.ui.icon.button').css({'background': ''})
+			$('.circular.ui.icon.button').disabled = false
 		}
 	}
 });
