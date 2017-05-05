867e2cca618e9822fcc69b539e6620d4dc68a8fe
Rena Liu <rliu14@mit.edu>
2017-05-05 19:42:54 -0400

    added propagation of stars

--- index.js
+++ index.js
@@ -10,7 +10,36 @@ var starsToAdd = 0;
 var seenBefore = [];
 
 var stars = 100;
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    console.log(ca)
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
 $(document).ready(function() {
+	var cookieStars = getCookie('stars')
+	if(cookieStars){
+		stars = parseInt(cookieStars)
+	}
 	refreshStars();
 });
 
@@ -64,6 +93,8 @@ $(document).on('click', '#bowtieButton', function() {
 
 
 function refreshStars() {
+	setCookie('stars', stars, 1)
+
 	$("#starText").html(stars);
 }
 
--- login.js
+++ login.js
@@ -1,14 +1,47 @@
 var order = undefined
 var password = "0123"
+var user = ''
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    console.log(ca)
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 $(document).on("click", ".login.button", function(evt) {
 	order = undefined
+	user = this.id
 	$('.ui.modal').modal('show');
+	$('#username').html(user);
 });
 
 $(document).on("click", '.circular.ui.icon.button', function(evt) {
+
+	var x = getCookie('username')
+		if (x) {
+			console.log(x)
+		} else {
+			console.log("nope")
+		}
+
 	var button_id = evt.target.id
-	console.log(button_id)
 	$('#' + button_id).css({'background': 'red'})
 	$('#' + button_id).prop('disabled', true);
 	if (order == undefined) {
@@ -21,6 +54,7 @@ $(document).on("click", '.circular.ui.icon.button', function(evt) {
 	if (order.length == 4) {
 		if (order == password) {
 			$('.circular.ui.icon.button').css({'background': 'green'})
+			setCookie('username', user, 1)
 			setTimeout(function () {
 				window.location.href='index.html';
 			}, 500)
